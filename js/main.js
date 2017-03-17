2a719121844d45ece7f1662cf7172f37259add29
GerwinChan <gerwinschool88@gmail.com>
2017-03-17 11:53:11 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -9,14 +9,42 @@
  * Opdracht 1: primitives
  * Maak voor de volgende primitives 5 goed bedachte variabelen per primitive
  * 
- * 5 variabelen met daarin een string
- * 5 variabelen met daarin een number
- * 5 variabelen met daarin een boolean
- * 
+ *
+ *
  * Geef ze een goede engelse naam en sla er iets in op wat een goed voorbeeld geeft
  * van Media Development producten (apps, sites, etc.)
  * Bijvoorbeeld een string variabele met de naam 'teacherName' en als waarde 'Berend'
  */
+// 5 variabelen met daarin een string
+
+    var Schoolname = "Media College";
+    var Brand = "Nike";
+    var Street = "Kalverstraat";
+    var Movie = "Fast and furious";
+    var OwnName = "Gerwin Chan";
+
+
+
+
+  //5 variabelen met daarin een number
+
+    var Age = 17;
+    var People = 21;
+    var Schools = 2;
+    var Grade = 10;
+    var Size = 8;
+
+
+
+  //5 variabelen met daarin een boolean
+
+    var x = true;
+    var y = false;
+    var Random = true;
+    var Homework = true;
+    var Weather = false;
+
+
 
 /**
  * Opdracht 2: Arrays
@@ -24,7 +52,20 @@
  * console.log de lengte van de arrays
  *
  * Bijvoorbeeld een Array met de naam 'students' en als waarde ['Berend', "Erwin']
+
+
+
+
+
  */
+   var students = ["Jordy","Nino","Wesly"];
+   var schools = ["Media College","Horizon College","Amstelveen College"];
+   var brands = ["Nike","Adidas","Iphone"];
+
+   console.log(students.length);
+   console.log(schools.length);
+   console.log(brands.length);
+
 
 /**
  * Opdracht 3: Arrays push
@@ -32,12 +73,26 @@
  * push er een string in
  */
 
+ var brands = [1,2,3,4];
+
+ brands.push("kiwi");
+
+ console.log(brands);
 /**
  * Opdracht 4: Array splice
  * Maak een array aan en vul hem met namen
  * Gooi de naam op de 1e plek weg
  * Gooi de naam op de 3e plek weg
  */
+
+ var fruit = ["apple","mango","peer"];
+
+ fruit.splice(0,1);
+ fruit.splice(1,1
+ );
+ console.log(fruit);
+
+
 
 /**
  * Opdracht 5: Objects
